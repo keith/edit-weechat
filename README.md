2f6f7e1701cdfc776e469d55c374a2c414e44894
Keith Smiley <keithbsmiley@gmail.com>
2015-10-18 19:24:28 -0700

    Initial Commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# edit-weechat
+
+This simple [weechat](https://weechat.org/) plugin allows you to
+compose messages in your `$EDITOR`.
+
+# Usage
+
+```sh
+/edit
+# Type some stuff
+# Save and quit
+```
+
+# Installation
+
+Copy the script to `~/.weechat/python/autoload`
+
+```
+mkdir -p ~/.weechat/python/autoload
+wget https://raw.githubusercontent.com/keith/edit-weechat/master/edit ~/.weechat/python/autoload
+```
---
+++ edit.py
@@ -0,0 +1,46 @@
+# Open your $EDITOR to compose a message in weechat
+#
+# Usage:
+# /edit
+#
+# History:
+# 10-18-2015
+# Version 1.0.0: initial release
+
+import os
+import os.path
+import subprocess
+import weechat
+
+
+def edit(data, buf, args):
+    editor = os.environ.get("EDITOR", "vim")
+    path = os.path.expanduser("~/.weechat/message.txt")
+    open(path, "w+")
+
+    cmd = [editor, path]
+    code = subprocess.Popen(cmd).wait()
+    if code != 0:
+        weechat.command(buf, "/window refresh")
+        return weechat.WEECHAT_RC_ERROR
+
+    with open(path) as f:
+        text = f.read()
+        weechat.buffer_set(buf, "input", text)
+        weechat.buffer_set(buf, "input_pos", str(len(text)))
+
+    weechat.command(buf, "/window refresh")
+
+    return weechat.WEECHAT_RC_OK
+
+
+def main():
+    if not weechat.register("edit", "Keith Smiley", "1.0.0", "MIT",
+                            "Open your $EDITOR to compose a message", "", ""):
+        return weechat.WEECHAT_RC_ERROR
+
+    weechat.hook_command("edit", "Open your $EDITOR to compose a message", "",
+                         "", "", "edit", "")
+
+if __name__ == "__main__":
+    main()
