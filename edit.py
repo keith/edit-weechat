f5a8ab1e8345a8b13d7c13445968b66addbbe1bb
W. <graywolf@users.noreply.github.com>
2025-06-25 16:07:41 +0000

    Improve support for external editing (#14)
    * Drop terminal option
    
    The terminal option had few issues, mainly with always passing the -e.
    It added needless complexity for the cases, when running gvim is enough.
    So let's just drop that option completely and instead expect user to
    correctly configure editor, for example using:
    
      /set plugins.var.python.edit.editor "gvim -f"
      /set plugins.var.python.edit.run_externally "true"
    
    * Use temporary files for the message
    
    With the previous implementation, there was a limit of being able to
    edit only one message at one time (since the file name was hard-coded).
    This commit instead uses a tempfile package to generate unique file for
    each message, meaning you can edit multiple messages at the same time
    (arguably only useful with run_externally = true).

--- edit.py
+++ edit.py
@@ -5,19 +5,30 @@
 #
 # Optional settings:
 # /set plugins.var.python.edit.editor "vim -f"
-# /set plugins.var.python.edit.terminal "xterm"
 # /set plugins.var.python.edit.run_externally "false"
 #
+# If run_externally, the editor is spawned without blocking weechat. The
+# process should not output to the terminal (use GUI program or spawn a new
+# terminal). Otherwise the editor is executed in the current terminal (blocking
+# weechat, which can break stuff).
+#
 # History:
+# 2022-09-09
+# Version 1.0.4: Use temporary files for the message
+# Version 1.0.3: Drop terminal option and leave that up to the user
 # 10-18-2015
 # Version 1.0.2: Add the ability to run the editor in a external terminal
 # Version 1.0.1: Add configurable editor key
 # Version 1.0.0: initial release
 
+VERSION = "1.0.4"
+
+import json
 import os
 import os.path
 import shlex
 import subprocess
+import tempfile
 import weechat
 
 def xdg_cache_dir():
@@ -30,14 +41,11 @@ def weechat_cache_dir():
     return os.path.expanduser(os.environ.get("WEECHAT_HOME", "~/.weechat/"))
 
 
-PATH = os.path.join(weechat_cache_dir(), "message.txt")
-
-
 def editor_process_cb(data, command, return_code, out, err):
-    buf = data
+    buf, path = json.loads(data)
 
     if return_code != 0:
-        cleanup(PATH, buf)
+        cleanup(path, buf)
         weechat.prnt("", "{}: {}".format(
             err.strip(),
             return_code
@@ -45,8 +53,8 @@ def editor_process_cb(data, command, return_code, out, err):
         return weechat.WEECHAT_RC_ERROR
 
     if return_code == 0:
-        read_file(PATH, buf)
-        cleanup(PATH, buf)
+        read_file(path, buf)
+        cleanup(path, buf)
 
     return weechat.WEECHAT_RC_OK
 
@@ -62,8 +70,9 @@ def cleanup(path, buf):
 
 def read_file(path, buf):
     try:
-        with open(PATH) as f:
+        with open(path) as f:
             text = f.read().strip()
+
         weechat.buffer_set(buf, "input", text)
         weechat.buffer_set(buf, "input_pos", str(len(text)))
 
@@ -73,13 +82,11 @@ def read_file(path, buf):
     weechat.command(buf, "/window refresh")
 
 
-def hook_editor_process(terminal, editor, path, buf):
-    term_cmd = "{} -e".format(terminal)
+def hook_editor_process(editor, path, buf):
     editor_cmd = "{} {}".format(editor, path)
-    weechat.hook_process("{} \"{}\"".format(
-        term_cmd,
-        editor_cmd
-    ), 0, "editor_process_cb", buf)
+    data = json.dumps([buf, path])
+    weechat.hook_process(
+        shlex.join(shlex.split(editor) + [path]), 0, "editor_process_cb", data)
 
 
 def run_blocking(editor, path, buf):
@@ -96,29 +103,27 @@ def edit(data, buf, args):
     editor = (weechat.config_get_plugin("editor")
               or os.environ.get("EDITOR", "vim -f"))
 
-    terminal = (weechat.config_get_plugin("terminal")
-                or os.getenv("TERMCMD"))
-
-    terminal = terminal or "xterm"
-
     run_externally = weechat.config_string_to_boolean(
         weechat.config_get_plugin("run_externally")
     )
     run_externally = bool(run_externally)
 
-    with open(PATH, "w+") as f:
+    f, path = tempfile.mkstemp(prefix = "weechat-edit-")
+    os.close(f)
+
+    with open(path, "w+") as f:
         f.write(weechat.buffer_get_string(buf, "input"))
 
     if run_externally:
-        hook_editor_process(terminal, editor, PATH, buf)
+        hook_editor_process(editor, path, buf)
     else:
-        run_blocking(editor, PATH, buf)
+        run_blocking(editor, path, buf)
 
     return weechat.WEECHAT_RC_OK
 
 
 def main():
-    if not weechat.register("edit", "Keith Smiley", "1.0.0", "MIT",
+    if not weechat.register("edit", "Keith Smiley", VERSION, "MIT",
                             "Open your $EDITOR to compose a message", "", ""):
         return weechat.WEECHAT_RC_ERROR
 
